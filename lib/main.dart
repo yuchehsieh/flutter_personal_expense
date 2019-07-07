0e8ca7acf98d582e90a4d24c84c0c8fe55a4a657
yuchehseih <hsiehdanny860605@gmail.com>
2019-07-07 15:18:47 +0800

    learn: Widget combination for positioning, sizing (Card, Text, Column), there always many ways to achieve same goal

--- lib/main.dart
+++ lib/main.dart
@@ -16,12 +16,25 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter App'),
-      ),
-      body: Center(
-        child: Text('Widget Playground!'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Flutter App'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Card(
+              margin: EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 50),
+              child: Container(
+                width: double.infinity,
+                padding: EdgeInsets.only(top: 50, bottom: 50),
+                alignment: Alignment.center,
+                child: Text('CHART HERE'),
+              ),
+              elevation: 5,
+            ),
+            Card(
+              child: Text('List of TX'),
+            )
+          ],
+        ));
   }
 }
