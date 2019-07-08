c835ec2cbb35ab7dcda6d53618237200f14798ef
yuchehseih <hsiehdanny860605@gmail.com>
2019-07-08 16:57:27 +0800

    learn: use showModalBottomSheet to have the modal raised up from bottom, still some issuegit add .

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-import 'package:personal_expense/widgets/user_transactions.dart';
+import 'package:personal_expense/widgets/new_transaction.dart';
+import 'package:personal_expense/widgets/transaction_list.dart';
+import 'models/transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,14 +16,54 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _userTransaction = [
+    Transaction(
+        id: 't1', amount: 69.99, title: 'New Shoes', date: DateTime.now()),
+    Transaction(id: 't2', amount: 19.99, title: 'Dinner', date: DateTime.now()),
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+      amount: txAmount,
+      date: DateTime.now(),
+      id: DateTime.now().toString(),
+      title: txTitle,
+    );
+    setState(() {
+      _userTransaction.add(newTx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (bCtx) {
+          return GestureDetector(
+            onTap: () {},
+            child: NewTransaction(_addNewTransaction),
+            behavior: HitTestBehavior.opaque,
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter App'),
         actions: <Widget>[
-          IconButton(icon: Icon(Icons.add), onPressed: () {}),
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              _startAddNewTransaction(context);
+            },
+          ),
         ],
       ),
       body: SingleChildScrollView(
@@ -38,13 +80,15 @@ class MyHomePage extends StatelessWidget {
               ),
               elevation: 3,
             ),
-            UserTransactions()
+            TransactionList(userTransaction: _userTransaction)
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
         child: Icon(Icons.add),
+        onPressed: () {
+          _startAddNewTransaction(context);
+        },
       ),
 //      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
