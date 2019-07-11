5b2d4b2dc8e3dcfcf01712e78a889bf829f63006
yuchehseih <hsiehdanny860605@gmail.com>
2019-07-11 18:31:04 +0800

    learn: introduce the ListTile<Widget> and used to display list of transaction

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -34,59 +34,84 @@ class TransactionList extends StatelessWidget {
           : ListView.builder(
               itemBuilder: (ctx, index) {
                 return Card(
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                  child: Container(
-                      width: double.infinity,
-                      padding:
-                          EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-                      child: Row(
-                        children: <Widget>[
-                          Container(
-                            padding: EdgeInsets.all(10),
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                color: Theme.of(context).primaryColor,
-                                width: 1.0,
-                              ),
-//                                      color: Colors.deepOrange,
-                            ),
-                            child: Text(
-                              '\$ ${userTransaction[index].amount.toStringAsFixed(2)}',
-                              textAlign: TextAlign.start,
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 15,
-                                  color: Theme.of(context).primaryColorDark),
-                            ),
+                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                  elevation: 5,
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(6.0),
+                        child: FittedBox(
+                          child: Text(
+                            '\$${userTransaction[index].amount.toStringAsFixed(2)}',
                           ),
-                          Expanded(
-                            child: Container(
-                              padding: EdgeInsets.only(left: 40),
-//                                      color: Colors.amberAccent,
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                  Text(
-                                    userTransaction[index].title,
-                                    textAlign: TextAlign.start,
-                                    style: Theme.of(context).textTheme.title,
-                                  ),
-                                  Text(
-                                    DateFormat.yMMMd()
-                                        .format(userTransaction[index].date),
-                                    textAlign: TextAlign.start,
-                                    style: TextStyle(
-                                      color: Colors.grey,
-                                      fontSize: 14,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          )
-                        ],
-                      )),
+                        ),
+                      ),
+                    ),
+                    title: Text(
+                      userTransaction[index].title,
+                      textAlign: TextAlign.start,
+                      style: Theme.of(context).textTheme.title,
+                    ),
+                    subtitle: Text(
+                      DateFormat.yMMMd().format(userTransaction[index].date),
+                    ),
+                  ),
                 );
+//                  Card(
+//                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+//                  child: Container(
+//                      width: double.infinity,
+//                      padding:
+//                          EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+//                      child: Row(
+//                        children: <Widget>[
+//                          Container(
+//                            padding: EdgeInsets.all(10),
+//                            decoration: BoxDecoration(
+//                              border: Border.all(
+//                                color: Theme.of(context).primaryColor,
+//                                width: 1.0,
+//                              ),
+////                                      color: Colors.deepOrange,
+//                            ),
+//                            child: Text(
+//                              '\$ ${userTransaction[index].amount.toStringAsFixed(2)}',
+//                              textAlign: TextAlign.start,
+//                              style: TextStyle(
+//                                  fontWeight: FontWeight.w500,
+//                                  fontSize: 15,
+//                                  color: Theme.of(context).primaryColorDark),
+//                            ),
+//                          ),
+//                          Expanded(
+//                            child: Container(
+//                              padding: EdgeInsets.only(left: 40),
+////                                      color: Colors.amberAccent,
+//                              child: Column(
+//                                crossAxisAlignment: CrossAxisAlignment.start,
+//                                children: <Widget>[
+//                                  Text(
+//                                    userTransaction[index].title,
+//                                    textAlign: TextAlign.start,
+//                                    style: Theme.of(context).textTheme.title,
+//                                  ),
+//                                  Text(
+//                                    DateFormat.yMMMd()
+//                                        .format(userTransaction[index].date),
+//                                    textAlign: TextAlign.start,
+//                                    style: TextStyle(
+//                                      color: Colors.grey,
+//                                      fontSize: 14,
+//                                    ),
+//                                  ),
+//                                ],
+//                              ),
+//                            ),
+//                          )
+//                        ],
+//                      )),
+//                );
               },
               itemCount: userTransaction.length,
             ),
