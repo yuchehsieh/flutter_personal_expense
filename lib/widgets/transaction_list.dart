a77bb58debe33dd292a131407880e4bae3880fa6
yuchehseih <hsiehdanny860605@gmail.com>
2019-07-13 12:46:04 +0800

    learn: could not use Expanded<Widget> wrap outside the ListView<Widget>, it need to calculate the available height and pass into 'height'

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -15,7 +15,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300,
+      height: 600,
       child: userTransaction.isEmpty
           ? Column(
               children: <Widget>[
