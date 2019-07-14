b2f47bbd945226df39d4d4053cefb946dc6c9eae
yuchehseih <hsiehdanny860605@gmail.com>
2019-07-14 12:01:20 +0800

    learn: get the current device width and decide the content, also FlatButton.icon()<Widget> render icon with lable

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -65,13 +65,22 @@ class TransactionList extends StatelessWidget {
                   subtitle: Text(
                     DateFormat.yMMMd().format(userTransaction[index].date),
                   ),
-                  trailing: IconButton(
-                    icon: Icon(Icons.delete),
-                    color: Theme.of(context).errorColor,
-                    onPressed: () {
-                      deleteTransaction(userTransaction[index].id);
-                    },
-                  ),
+                  trailing: MediaQuery.of(context).size.width > 460
+                      ? FlatButton.icon(
+                          icon: Icon(Icons.delete),
+                          label: Text('Delete'),
+                          textColor: Theme.of(context).errorColor,
+                          onPressed: () {
+                            deleteTransaction(userTransaction[index].id);
+                          },
+                        )
+                      : IconButton(
+                          icon: Icon(Icons.delete),
+                          color: Theme.of(context).errorColor,
+                          onPressed: () {
+                            deleteTransaction(userTransaction[index].id);
+                          },
+                        ),
                 ),
               );
 //                  Card(
