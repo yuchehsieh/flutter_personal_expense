04274b8e61fdfa490da5229b1207ac04d7c01055
yuchehseih <hsiehdanny860605@gmail.com>
2019-07-14 12:26:18 +0800

    learning: get current Platform, and some widget has the special constructor for more adaptive design, Switch.adaptive()<Widget>

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 // import 'package:flutter/services.dart';
 import 'package:personal_expense/widgets/chart.dart';
@@ -148,7 +150,8 @@ class _MyHomePageState extends State<MyHomePage> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     Text('Show Chart'),
-                    Switch(
+                    Switch.adaptive(
+                      activeColor: Theme.of(context).accentColor,
                       value: _showChart,
                       onChanged: (val) {
                         setState(() {
@@ -177,12 +180,14 @@ class _MyHomePageState extends State<MyHomePage> {
               if (!isLandscape) txListWidget
             ]),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          _startAddNewTransaction(context);
-        },
-      ),
+      floatingActionButton: Platform.isIOS
+          ? Container()
+          : FloatingActionButton(
+              child: Icon(Icons.add),
+              onPressed: () {
+                _startAddNewTransaction(context);
+              },
+            ),
 //      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
