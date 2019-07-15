f62ec127f93aa0f88ac4f72eddf51e373146e64f
yuchehseih <hsiehdanny860605@gmail.com>
2019-07-15 15:06:01 +0800

    learn: const constructor / const [CLASS_NAME] could prevent unnecessary rebuild, it's important

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -25,7 +25,7 @@ class TransactionList extends StatelessWidget {
                   'No transactions added yet!',
                   style: Theme.of(context).textTheme.title,
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 10,
                 ),
                 Container(
@@ -67,15 +67,15 @@ class TransactionList extends StatelessWidget {
                   ),
                   trailing: MediaQuery.of(context).size.width > 460
                       ? FlatButton.icon(
-                          icon: Icon(Icons.delete),
-                          label: Text('Delete'),
+                          icon: const Icon(Icons.delete),
+                          label: const Text('Delete'),
                           textColor: Theme.of(context).errorColor,
                           onPressed: () {
                             deleteTransaction(userTransaction[index].id);
                           },
                         )
                       : IconButton(
-                          icon: Icon(Icons.delete),
+                          icon: const Icon(Icons.delete),
                           color: Theme.of(context).errorColor,
                           onPressed: () {
                             deleteTransaction(userTransaction[index].id);
